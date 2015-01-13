68aeb32fd167c35aee4a80f6b5df77cd9e912c73
firstuser <firstuser@bbb-desktop>
2015-01-13 11:56:34 +0530

    change url paths

--- sample/src/main/java/com/mkyong/common/controller/CreateMeetingController.java
+++ sample/src/main/java/com/mkyong/common/controller/CreateMeetingController.java
@@ -1,6 +1,8 @@
 package com.mkyong.common.controller;
 
 import com.relecotech.bbb.api.APIGenerator;
+import com.relecotech.bbb.api.XmlParser;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.xml.parsers.ParserConfigurationException;
@@ -12,31 +14,31 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping("/meeting")
-public class MovieController {
+@RequestMapping("/createmeeting")
+public class CreateMeetingController {
 
     @RequestMapping(value = "/{name}", method = RequestMethod.GET)
     //public String getMovie(@PathVariable String name, ModelMap model) {
-    public @ResponseBody ModelAndView getMovie(@PathVariable String name) throws ParserConfigurationException {
+    public @ResponseBody String getMovie(@PathVariable String name) throws ParserConfigurationException {
         String ss = "failed";
         try {
             //model.addAttribute("meeting", name);
             APIGenerator aPIGenerator = new APIGenerator();
-            //Map<String, String> responceMap = XmlParser.runAPI(aPIGenerator.createAPI("join", name));
-            aPIGenerator.createAPI("join", name);
+            Map<String, String> responceMap = XmlParser.runAPI( aPIGenerator.createAPI("create", name));
+           // aPIGenerator.createAPI("join", name);
             // ss = responceMap.get("meetingID");
-            System.out.println("in the class.....value=" + aPIGenerator.apiWithChecksum);
+          //  System.out.println("in the class.....value=" + aPIGenerator.apiWithChecksum);
             // aPIGenerator.apiWithChecksum;
-            return new ModelAndView("redirect:" + aPIGenerator.apiWithChecksum);  
+           // return new ModelAndView("redirect:" + aPIGenerator.apiWithChecksum);  
 //        } catch (SAXException ex) {
 //            Logger.getLogger(MovieController.class.getName()).log(Level.SEVERE, null, ex);
 //        } catch (TransformerException ex) {
 //            Logger.getLogger(MovieController.class.getName()).log(Level.SEVERE, null, ex);
         } catch (Exception ex) {
-            Logger.getLogger(MovieController.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(CreateMeetingController.class.getName()).log(Level.SEVERE, null, ex);
         }
        //return new ModelAndView("redirect:" + aPIGenerator.apiWithChecksum);
-return null;
+return name;
     }
     
 
